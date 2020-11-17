48924374e93be77bc961a48dd8ddf157efd9ba65
Mypaxa4e <ivanshtypula@gmail.com>
2020-11-17 16:14:49 +0200

    again

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Main from "../Containers/Main/Main";
-import NotFound from "../Containers/NotFound/NotFound";
+// import NotFound from "../Containers/NotFound/NotFound";
 
 import css from "./App.module.css";
 
@@ -11,8 +11,8 @@ function App() {
       <Switch>
         <Route exact path="/" component={Main} />
         <Route exact path="/schedule" component={Main} />
-        <Route component={NotFound}/>
-        {/* <Redirect to="/" /> */}
+        {/* <Route component={NotFound}/> */}
+        <Redirect to="/" />
       </Switch>
     </div>
   );
