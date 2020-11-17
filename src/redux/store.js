bf71c2018fa88e4901de79919a47f0180bb71f77
Mypaxa4e <ivanshtypula@gmail.com>
2020-11-17 15:08:48 +0200

    not found again

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,5 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import storage from "redux-persist/lib/storage";
-import thunk from "redux-thunk"; 
 import {
   persistStore,
   persistReducer,
@@ -18,15 +17,17 @@ const persistConfig = {
   storage,
 };
 
-const defaultMiddleware = getDefaultMiddleware({
+const middleware = getDefaultMiddleware({
+  immutableCheck: false,
   serializableCheck: {
     ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
   },
+  thunk: true,
 });
 
 const store = configureStore({
   reducer: persistReducer(persistConfig, rootReducer),
-  middleware: [...defaultMiddleware, thunk],
+  middleware,
 });
 
 export const persistor = persistStore(store);
